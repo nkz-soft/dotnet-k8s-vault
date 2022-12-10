e078c05d664f1c1fbe010bef7679c10f3ec9e9dd
NickolayZ <nczsoft@gmail.com>
2022-12-10 16:06:41 +0300

    rendering without template

--- src/VaultIntegrationService/Program.cs
+++ src/VaultIntegrationService/Program.cs
@@ -1,8 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHealthChecks();
 
-var app = builder.Build();
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddJsonFile("/vault/secrets/appsettings.json", optional: true, reloadOnChange: true);
 
+var app = builder.Build();
 
 app.MapHealthChecks("/healthz");
 app.MapGet("/config",
